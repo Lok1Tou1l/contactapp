cc63997b3aa51008566e5e0d83bf0d99947348e2
AnisDhia <aanisgamer@gmail.com>
2023-06-03 20:04:46 +0100

    Update #2 added groups and favourites screens Navigation bar works Login Button works

---
+++ lib/view/screens/groups_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class GrouopScreen extends StatefulWidget {
+  const GrouopScreen({super.key});
+
+  @override
+  State<GrouopScreen> createState() => _GrouopScreenState();
+}
+
+class _GrouopScreenState extends State<GrouopScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/view/widgets/navigation_widget.dart
+++ lib/view/widgets/navigation_widget.dart
@@ -1,4 +1,6 @@
 import 'package:contactapp/view/screens/contacts_screen.dart';
+import 'package:contactapp/view/screens/favorites_screen.dart';
+import 'package:contactapp/view/screens/groups_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -13,6 +15,12 @@ class Navigation extends StatefulWidget {
 class _NavigationState extends State<Navigation> {
   int _selectedIndex = 0;
   PageController _pageController = PageController();
+
+  final List<Widget> _screens = const [
+    ContactScreen(),
+    GrouopScreen(),
+    FavoriteScreen(),
+  ];
 
   @override
   void dispose() {
@@ -32,7 +40,15 @@ class _NavigationState extends State<Navigation> {
         appBar: AppBar(
           title: Text('Contacts'),
         ),
-        body: ContactScreen(),
+        body: PageView(
+          controller: _pageController,
+          children: _screens,
+          onPageChanged: (index) {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+        ),
         bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
@@ -48,9 +64,16 @@ class _NavigationState extends State<Navigation> {
               label: 'Favorites',
             ),
           ],
-          currentIndex: 0,
+          currentIndex: _selectedIndex,
           selectedItemColor: Colors.redAccent,
-          onTap: (index) {},
+          onTap: (index) {
+            _pageController.animateToPage(index,
+                duration: const Duration(milliseconds: 200),
+                curve: Curves.easeIn);
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
         ));
   }
 }
